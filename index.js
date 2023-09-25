ecebeb422b113768add53453e2f373b40eb8d5cf
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-09-25 19:29:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYearObj = record.find(game => game.result === "W");
+    return winningYearObj ? winningYearObj.year : undefined;
+  }
+  
